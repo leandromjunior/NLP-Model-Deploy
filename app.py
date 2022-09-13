d034db6855b4647ffd97cf34cd7d556097dc94df
unknown <lemottajr@hotmail.com>
2022-09-12 22:51:40 -0300

    committing files

---
+++ app.py
@@ -0,0 +1,21 @@
+import json
+from flask import Flask, jsonify, request
+from joblib import load
+
+app = Flask(__name__)
+
+@app.route("/")
+
+#Verificando se um parâmetro foi enviado para o modelo
+def index():
+    if "query" not in request.args:
+        return jsonify({"prediction" : None, "message" : "send me a text"})
+
+    query = request.args.get("query")
+    model = load("model.joblib")
+    labels = ['carros', 'economia', 'educacao', 'esporte', 'musica', 'politica']
+
+    predict = model.predict([query])
+    prediction = labels[predict[0]]
+
+    return jsonify({"prediction" : prediction})
